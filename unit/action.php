e61e97c69af4e3005687b46f881cd86d3457aa12
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-12-10 15:32:05 +0900

    New: unit/action.php - Testcase of Unit are included with each unit.

---
+++ unit/action.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * module-testcase:/unit/action.php
+ *
+ * @created   2019-12-09
+ * @version   1.0
+ * @package   module-testcase
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ * @created   2019-02-20
+ */
+namespace OP;
+
+/* @var $args array */
+$name = array_shift($args);
+
+//	...
+Unit::Singleton($name)->Testcase($args);
