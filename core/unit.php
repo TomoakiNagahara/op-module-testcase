6dd3e40fce0f10e97dcddc00f1fad4cf834178a4
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-12-13 17:52:44 +0900

    New: core/unit.php

--- core/unit.php
+++ core/unit.php
@@ -1,1 +1,18 @@
 <?php
+/**
+ * module-testcase:/core/unit.php
+ *
+ * @created   2019-12-13
+ * @version   1.0
+ * @package   module-testcase
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ * @created   2019-12-13
+ */
+namespace OP;
+
+D();
