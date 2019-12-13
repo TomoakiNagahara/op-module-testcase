bbd505ca6b613562b81e8ec315ae0c4537c77136
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-12-13 17:52:59 +0900

    New: Testcase.class.php

---
+++ Testcase.class.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * module-testcase:/Testcase.class.php
+ *
+ * @created   2019-12-13
+ * @version   1.0
+ * @package   module-testcase
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ * @created   2019-12-13
+ */
+namespace OP;
+
+/** use
+ *
+ * @created   2019-12-13
+ */
+
+/** Testcase
+ *
+ * @created   2019-12-13
+ * @version   1.0
+ * @package   module-testcase
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+class Testcase implements IF_UNIT
+{
+	/** use trait
+	 *
+	 */
+	use OP_UNIT;
+}
