469db46cb1f9fa6b109319cae5248621ea9ff608
Tomoaki mbp13r-2015 <tomoaki.nagahara@gmail.com>
2020-05-22 18:22:36 +0900

    Fix: index.php | Correspond to shell.

--- index.php
+++ index.php
@@ -35,20 +35,3 @@ RootPath('testcase', $root_path);
 
 //	...
 $app->Template('index.phtml');
-
-//	...
-$app->Template('controller.php');
-
-//	...
-echo '<hr/>'.PHP_EOL;
-
-//	...
-Html(null, 'hr');
-Html( 'momory usage: ' . memory_get_usage() / 1000 .'KB (max: '. memory_get_peak_usage() / 1000 .'KB)');
-Debug::Set('PHP' , PHP_VERSION);
-Debug::Set('SAPI', php_sapi_name());
-Debug::Set('OP'  , Env::isLocalhost() ? 'localhost': geoip_country_code_by_name('onepiece-framework.com'));
-Debug::Set('test',true);
-Debug::Set('test',false);
-Debug::Set('test',null);
-Debug::Out();
