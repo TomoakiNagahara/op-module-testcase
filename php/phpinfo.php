ddf61c12e209078c34ca6f19293a5c8703392a1d
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2021-01-06 16:14:29 +0900

    New: php > phpinfo.php

---
+++ php/phpinfo.php
@@ -0,0 +1,20 @@
+<?php
+/** op-module-testcase:/php/phpinfo.php
+ *
+ * @created   2020-01-06
+ * @version   1.0
+ * @package   op-module-testcase
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+D('session_save_path', session_save_path());
+
+//	...
+phpinfo();
