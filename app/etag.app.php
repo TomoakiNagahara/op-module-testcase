e81b382886dff5178a28105eda580e027e836e0d
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-01-07 18:22:36 +0900

    Del: app > *

--- app/etag.app.php
+++
@@ -1,9 +0,0 @@
-<?php
-/** op-module-testcase:/app/etag.app.php
- *
- * @created   2019-05-08
- * @version   1.0
- * @package   op-module-testcase
- * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
- * @copyright Tomoaki Nagahara All right reserved.
- */
